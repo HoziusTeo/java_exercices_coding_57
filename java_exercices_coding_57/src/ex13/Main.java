7d23e7b83a40cd6094e874f9162e2af3ad10ef6c
hozat <hoza.teodor@yahoo.com>
2023-05-20 15:53:17 +0300

    Finished classes

--- java_exercices_coding_57/src/ex13/Main.java
+++ java_exercices_coding_57/src/ex13/Main.java
@@ -1,7 +1,43 @@
 package ex13;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String word = "fundamentals";
+        String[] player = new String[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            if (i == 0 || i == word.length() - 1) {
+                System.out.print(word.charAt(i));
+                player[i] = word.charAt(i) + "";
+            } else {
+                player[i] = "_";
+                System.out.print("_");
+            }
+            System.out.print(" ");
+        }
+        Scanner scanner = new Scanner(System.in);
 
+        do {
+            String userinput = scanner.next();
+            if (word.contains(userinput)) {
+                for(int i = 0;i < word.length();i++){
+                    char c = word.charAt(i);
+                    if (c == userinput.charAt(0)){
+                        System.out.println(userinput.charAt(0));
+                        player[i] = userinput;
+                    }
+                }
+                showArray(player);
+            } else {
+                System.out.println("Mai incearca");
+            }
+        } while (true);
+    }
+
+    public static void showArray(String[] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
     }
 }
